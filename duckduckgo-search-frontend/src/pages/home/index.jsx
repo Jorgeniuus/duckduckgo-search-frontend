c51af66f3306d481d631701d6a01f4824a211460
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2024-11-08 21:49:42 -0300

    style: CSS intial style

--- duckduckgo-search-frontend/src/pages/home/index.jsx
+++ duckduckgo-search-frontend/src/pages/home/index.jsx
@@ -4,11 +4,12 @@ import api from '../../services/api'
 import './styleHome.css'
 
 function App() {
-  let [searches, setSearches] = useState()
+  let [search, setSearche] = useState('')
+  let [searches, setSearches] = useState([])
 
     async function getSearches(){
       try{
-        const searchesFromApi = await api.get('/search?q=casas')
+        const searchesFromApi = await api.get(`/search?q=${search}`)
     
         setSearches(searchesFromApi.data)
       } catch (error) {
@@ -16,18 +17,70 @@ function App() {
       }
     } 
 
-    useEffect(() => {
-      getSearches();
-      console.log(searches); 
-    }, []);
+    const handleInputChangeSearch = (e) => {
+      setSearche(e.target.data)
+    }
+
+    // useEffect(() => {
+    //   getSearches();
+    //   console.log(searches); 
+    // }, []);
   
     useEffect(() => {
       console.log(searches);
     }, [searches]);
 
   return (
-    <div>
-
+    <div className='main-page'>
+      <nav className='input-searches'>
+        <input type="text"
+        placeholder='Search...'
+        value={search}
+        onChange={handleInputChangeSearch}
+        onKeyDown={(e) => e.key === 'Enter' && handleInputChangeSearch()}
+        />
+        <button onClick={getSearches}>Search</button>
+      </nav>
+      <div className='search-contents'>
+        <section className='page-searches'>
+          <div className='search-result'>
+            <a href="https://www.google.com.br/" target='_blank'>
+              <h3>Testando 1</h3>
+            </a>
+          </div>
+          <div className='search-result'>
+            <a href="https://www.google.com.br/" target='_blank'>
+              <h3>Testando 2</h3>
+            </a>
+          </div>
+          <div className='search-result'>
+            <a href="https://www.google.com.br/" target='_blank'>
+              <h3>Testando 3</h3>
+            </a>
+          </div>
+          {/* {searches.map(searchResult =>(
+            <div key={'1'}>
+              <h3>{searchResult.Text}</h3>
+              <a href={searchResult.FirstURL} target='_blank'>
+                {searchResult.FirstURL}
+              </a>
+            </div>
+          ))} */}
+        </section>
+        <aside className='side-history-bar'>
+          <h2 className='search-history-text'>Search History</h2>
+          <div className='history-result'>
+            <a href="https://www.google.com.br/" target='_blank'>
+              <h3>Testando 1</h3> 
+            </a>
+          </div>
+          <div className='history-result'>
+            <a href="https://www.google.com.br/" target='_blank'>
+              <h3>Testando 1</h3> 
+            </a>
+          </div>
+        </aside>
+      </div>
     </div>
   )
 }
